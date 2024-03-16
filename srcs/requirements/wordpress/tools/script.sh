5c317e9efcb21fdb43350433153216a26c46c8cc
Abdellah Azeroual <abazerou@e1r6p7.1337.ma>
2024-03-16 19:46:53 +0000

    DONE

---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Define the WordPress path
+WP_PATH="/var/www/html"
+
+# Update PHP configurations
+sed -i "s|listen = /run/php/php7.4-fpm.sock|listen = 9000|g" /etc/php/7.4/fpm/pool.d/www.conf
+
+
+# Check if the WordPress configuration file exists
+if [ ! -f "$WP_PATH/wp-config.php" ]; then
+    # Download wp-cli
+    echo "Downloading wp-cli..."
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+
+    # Download WordPress core
+    echo "Downloading WordPress core..."
+    wp core download --path="$WP_PATH" --allow-root
+
+    # Create wp-config.php file
+    echo "Creating wp-config..."
+    echo "Using DB_NAME: $MARIADB_DB"
+    echo "Using DB_USER: $MARIADB_USER"
+    echo "Using DB_PASSWORD: $MARIADB_PASS"
+    echo "Using DB_HOST: $DB_HOST"
+    wp config create --dbname="$MARIADB_DB" \
+                     --dbuser="$MARIADB_USER" \
+                     --dbpass="$MARIADB_PASS" \
+                     --dbhost="$DB_HOST" \
+                     --path="$WP_PATH" \
+                     --allow-root
+
+    # Install WordPress
+    echo "Installing WordPress..."
+    wp core install --url="$WP_URL" \
+                    --title="$WP_TITLE" \
+                    --admin_user="$WP_USER_LOGIN" \
+                    --admin_password="$WP_USER_PASSWORD" \
+                    --admin_email="$WP_USER_EMAIL" \
+                    --path="$WP_PATH" \
+                    --allow-root
+
+    echo "${GREEN}WORDPRESS INSTALLATION FINISHED.${NC}"
+else
+    echo "${GREEN}WORDPRESS IS ALREADY INSTALLED.${NC}"
+fi
+
+# Start PHP FPM
+/usr/sbin/php-fpm7.4 -F
