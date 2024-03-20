31b05acb8993cc7a554a63588e30a4e17f65fd9e
Abdellah Azeroual <abazerou@e1r6p7.1337.ma>
2024-03-20 18:06:09 +0000

    MODIFIED USER

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sleep 3
 
-# Replace 'your_root_password' with your actual root password
 mariadb -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 mariadb -e "CREATE USER IF NOT EXISTS 'abazerou'@'localhost' IDENTIFIED BY '1234';"
 mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'abazerou'@'%' IDENTIFIED BY '1234';"
